bc7d31c4c55bc1f5344abff8133ebaa4156a7e71
Daniel Souza <daniel.carlos.souza@gmail.com>
2017-12-22 18:29:29 -0200

    first commit

---
+++ ChatSignal/Hubs/ChatHub.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace ChatSignal.Hubs
+{
+
+    [HubName("ChatHub")]
+    public class ChatHub : Hub
+    {
+        public static List<string> Users = new List<string>();
+
+        public void Send(string nome, string message)
+        {
+            Clients.All.addMessage(nome, message);
+          /*  var context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+            context.Clients.All.updateUsersOnlineCount(count);
+            */
+        }
+
+       
+        public override Task OnConnected()
+        {
+            var clientId = GetClientId();
+
+            if (Users.IndexOf(clientId) == -1)
+            {
+                Users.Add(clientId);
+            }
+
+            // Send the current count of users
+            Send("Sistema", "Seja bem vindo....");
+
+            return base.OnConnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            var clientId = GetClientId();
+            if (Users.IndexOf(clientId) == -1)
+            {
+                Users.Add(clientId);
+            }
+
+            // Send the current count of users
+            //Send("Sistema", "Já está indo?", Users.Count);
+
+            return base.OnReconnected();
+        }
+
+        private string GetClientId()
+        {
+            string clientId = "";
+            if (Context.QueryString["clientId"] != null)
+            {
+                // clientId passed from application 
+                clientId = this.Context.QueryString["clientId"];
+            }
+
+            if (string.IsNullOrEmpty(clientId.Trim()))
+            {
+                clientId = Context.ConnectionId;
+            }
+
+            return clientId;
+        }
+    }
+}
